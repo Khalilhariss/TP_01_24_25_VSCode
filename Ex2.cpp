cc98645b41e5e6750ebf74c3eb580cf7d248b27a
Khalil Hariss <khalil.hariss1@usj.edu.lb>
2024-10-30 11:01:54 +0200

    Implement Ex2, Part 4: Error Message: Good  Solution

--- Ex2.cpp
+++ Ex2.cpp
@@ -4,18 +4,22 @@ using namespace std;
 int main()
 {
     int n;
+    bool bornSup;
+    bool bornInf;
     do{
     cout<<"Enter a positive positive integer and less than 70:";
     cin>>n;
-    if(n<=0)
+    bornInf=n<=0;
+    bornSup=n>=70;
+    if(bornInf)
     {
         cout<<"Your number is less  or equal to 0!"<<endl;
     }
-    if(n>=70)
+    if(bornSup)
     {
         cout<<"Your number is greater than or equal to 70!"<<endl;
     }
-    }while(n<=0 || n>=70);
+    }while(bornInf|| bornSup);
     for(int i=10; i>=0; --i)
     {
         cout<<n<<"x"<<i<<"="<<n*i<<endl;
