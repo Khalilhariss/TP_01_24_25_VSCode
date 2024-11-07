c21b3185ab36785337ad5ceaacda499765932feb
Khalil Hariss <khalil.hariss1@usj.edu.lb>
2024-11-07 19:10:58 +0200

    Implement Ex2, Part 2: An integer not less than or equal to 0

--- Ex2.cpp
+++ Ex2.cpp
@@ -4,8 +4,10 @@ using namespace std;
 int main()
 {
  int n;
+ do{
  cout<<"Enter a positive integer:"<<endl;
  cin>>n;
+ }while(n<=0);
  for(int i=10; i>=0; i--)
  {
     cout<<n<<"x"<<i<<"="<<n*i<<endl;
