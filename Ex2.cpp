db72939432ff7371767a364f3598b1699917775d
Khalil Hariss <khalil.hariss1@usj.edu.lb>
2024-11-07 19:10:13 +0200

    Implement Ex2 Simple multpilication Table

--- Ex2.cpp
+++ Ex2.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-    cout<<"Ex2"<<endl;
-    return 0;
-    }
+ int n;
+ cout<<"Enter a positive integer:"<<endl;
+ cin>>n;
+ for(int i=10; i>=0; i--)
+ {
+    cout<<n<<"x"<<i<<"="<<n*i<<endl;
+ }   
+ return 0;
+}
